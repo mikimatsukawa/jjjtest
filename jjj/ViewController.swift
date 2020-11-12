5c2410ce8a92b56a350d2c2aba0a2e0cc382cfad
MIKI HODEL <mikihodel99@gmail.com>
2020-11-11 21:49:59 -0600

    Initial Commit

---
+++ jjj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jjj
+//
+//  Created by Miki Hodel on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
